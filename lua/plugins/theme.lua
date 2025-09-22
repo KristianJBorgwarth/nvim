32cf94424fcef5a894d62b700999b6eeb51cd827
Kristian <10348902@pm.me>
2025-09-22 20:46:09 +0200

    theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,13 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine")
-	end
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      background = {
+        dark = "mocha",
+      },
+    })
+    vim.cmd("colorscheme catppuccin")
+  end
 }

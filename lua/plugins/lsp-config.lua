fa390c1c86e7a68b6cf2f210ef270fd20ba031c4
Kristian <10348902@pm.me>
2025-09-23 21:54:36 +0200

    csharp tabs and lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,34 +1,42 @@
 return {
-  { "neovim/nvim-lspconfig" },
-  { "williamboman/mason.nvim", build = ":MasonUpdate", config = true },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "neovim/nvim-lspconfig" },
-    opts = {
-      ensure_installed = { "lua_ls", "ts_ls", "omnisharp"},
-      handlers = {
-        function(server)
-          local cfg = vim.lsp.config(server)
-          vim.lsp.enable(cfg)
-        end,
-      },
-    },
-    config = function(_, opts)
-      require("mason-lspconfig").setup(opts)
-      vim.api.nvim_create_autocmd("LspAttach", { callback = function(ev)
-        local b = ev.buf
-        local o = { buffer = b, silent = true }
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, o)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, o)
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, o)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, o)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, o)
-        vim.keymap.set({ "n", "v" }, "<A-enter>", vim.lsp.buf.code_action, o)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, o)
-        vim.keymap.set({ "n", "v" }, "<leader>f", function() vim.lsp.buf.format({ async = false }) end, o)
-      end })
-    end,
-  },
+	{ "neovim/nvim-lspconfig" },
+	{ "williamboman/mason.nvim", build = ":MasonUpdate", config = true },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "neovim/nvim-lspconfig" },
+		opts = {
+			ensure_installed = { "lua_ls", "ts_ls", "omnisharp" },
+			handlers = {
+				function(server)
+					local cfg = vim.lsp.config(server)
+					vim.lsp.enable(cfg)
+				end,
+			},
+		},
+		["omnisharp"] = function()
+			require("lspconfig").omnisharp.setup({
+				root_dir = require("lspconfig.util").root_pattern("*.sln", "*.csproj", ".git"),
+				cmd = { "omnisharp" },
+			})
+		end,
+		config = function(_, opts)
+			require("mason-lspconfig").setup(opts)
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(ev)
+					local b = ev.buf
+					local o = { buffer = b, silent = true }
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, o)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, o)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, o)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, o)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, o)
+					vim.keymap.set({ "n", "v" }, "<A-enter>", vim.lsp.buf.code_action, o)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, o)
+					vim.keymap.set({ "n", "v" }, "<leader>f", function()
+						vim.lsp.buf.format({ async = false })
+					end, o)
+				end,
+			})
+		end,
+	},
 }
-
-
--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -1,7 +1,20 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+-- Default tabs: 2 spaces
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- C# tabs: 4 spaces
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cs",
+  callback = function()
+    vim.bo.expandtab = true
+    vim.bo.tabstop = 4
+    vim.bo.softtabstop = 4
+    vim.bo.shiftwidth = 4
+  end,
+})
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.number = true

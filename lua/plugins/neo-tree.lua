595cf5b9796877e34432f4c1af210c218ca8e583
Kristian <10348902@pm.me>
2025-09-23 22:14:50 +0200

    neo-tree remember op file

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,19 +1,30 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons",
-  },
-  lazy = false,
-  opts = {
-    window = { position = "left", },
-    filesystem = { hijack_netrw_behavior = "open_default" },
-  },
-  config = function(_, opts)
-    require("neo-tree").setup(opts)
-    vim.keymap.set("n", "<A-1>", "<cmd>Neotree toggle right<cr>", { silent = true, desc = "Toggle file explorer (left)" })
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	lazy = false,
+	opts = {
+		window = { position = "right" },
+		filesystem = {
+			hijack_netrw_behavior = "open_default",
+			follow_current_file = {
+				enabled = true,
+				leave_dirs_open = true,
+			},
+			use_libuv_file_watcher = true,
+		},
+	},
+	config = function(_, opts)
+		require("neo-tree").setup(opts)
+		vim.keymap.set(
+			"n",
+			"<A-1>",
+			"<cmd>Neotree toggle right<cr>",
+			{ silent = true, desc = "Neo-tree: toggle (right)" }
+		)
+	end,
 }
-

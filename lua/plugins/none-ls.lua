be8f0b030597be52c8fe4689aaaf9fe10d26037b
Kristian <10348902@pm.me>
2025-09-23 20:50:19 +0200

    completions and tabs non-satanic

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,9 +5,9 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.eslint,
+        null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.csharpier,
+	null_ls.builtins.formatting.csharpier
       }
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})

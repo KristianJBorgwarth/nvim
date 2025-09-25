90d5c71c1ab32d3c060829ca7267ee24e63e6e0e
Kristian <10348902@pm.me>
2025-09-25 23:40:44 +0200

    initial neogit cmds

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -4,4 +4,15 @@ return {
 		"nvim-lua/plenary.nvim",
 		"sindrets/diffview.nvim",
 	},
+	config = function()
+		local neogit = require("neogit")
+
+		vim.keymap.set("n", "<leader>ga<CR>", function()
+			vim.cmd("!git add .")
+		end, { silent = true, noremap = true })
+
+		vim.keymap.set("n", "<leader>ng", function()
+			neogit.open()
+		end, { silent = true, noremap = true, desc = "Open Neogit" })
+	end,
 }

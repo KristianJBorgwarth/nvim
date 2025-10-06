f741d6ea33af3e25b2382f03382bd1bd563fd0da
Kristian <10348902@pm.me>
2025-10-06 11:55:22 +0200

    low/up split nav

--- lua/vim-editor-commands.lua
+++ lua/vim-editor-commands.lua
@@ -7,10 +7,13 @@ map("n", "<A-S-s>", "<cmd>split<CR>", { desc = "Horizontal split", silent = true
 -- move between split windows
 map({ "n", "v" }, "<A-h>", "<C-w>h", { desc = "Move to left split" })
 map({ "n", "v" }, "<A-l>", "<C-w>l", { desc = "Move to right split" })
+map({ "n", "v" }, "<A-j>", "<C-w>j", { desc = "Move to lower split" })
+map({ "n", "v" }, "<A-k>", "<C-w>k", { desc = "Move to upper split" })
 map("i", "<A-h>", "<C-o><C-w>h", { desc = "Move to left split" })
 map("i", "<A-l>", "<C-o><C-w>l", { desc = "Move to right split" })
+map("i", "<A-j>", "<C-o><C-w>j", { desc = "Move to lower split" })
+map("i", "<A-k>", "<C-o><C-w>k", { desc = "Move to upper split" })
 
 -- close current window
 map({ "n", "v" }, "<A-q>", "<C-w>q", { desc = "Close current split" })
 map("i", "<A-q>", "<C-o><C-w>q", { desc = "Close current split" })
-

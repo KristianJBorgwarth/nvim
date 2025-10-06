11189c7ee8fc9fb1bbad15494cbca71a11aa9bbd
Kristian <10348902@pm.me>
2025-10-06 20:37:35 +0200

    improved test flow

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-neotest/neotest",
 	dependencies = {
-		"Issafalcon/neotest-dotnet",
+		"nsidorenco/neotest-vstest",
 		"nvim-neotest/nvim-nio",
 		"nvim-lua/plenary.nvim",
 		"antoinemadec/FixCursorHold.nvim",
@@ -20,17 +20,34 @@ return {
 			output = {
 				open_on_run = true,
 			},
+			discovery = {
+				filter_dir = function(name, rel_path, root)
+					local ignore = { "bin", "obj", "Helpers", ".git", "Setup", "Fixtures", "Authentication" }
+					for _, n in ipairs(ignore) do
+						if name == n then
+							return false
+						end
+					end
+					return true
+				end,
+				is_test_file = function(file_path)
+					if file_path:match("[/\\]Setup[/\\]") then
+						return false
+					end
+					local ends_with = function(pat)
+						return file_path:lower():match(pat) ~= nil
+					end
+					return ends_with("[Tt]ests?%.cs$")
+						or ends_with("[%.%-][Tt]est[s]?%.cs$")
+						or ends_with("[Ii]ntegration[Tt]ests?%.cs$")
+				end,
+			},
 			adapters = {
-				require("neotest-dotnet")({
-					dap = {
-						adapter_name = "coreclr",
-						justMyCode = false,
+				require("neotest-vstest")({
+					args = { "-l:console;verbosity=minimal", "--nologo" },
+					dap_settings = {
+						type = "netcoredbg",
 					},
-					dotnet_additional_args = {
-						"--verbosity detailed",
-            "--configuration", "Debug",
-					},
-					discovery_root = "solution",
 				}),
 			},
 		})
@@ -41,17 +58,24 @@ return {
 			nt.summary.toggle()
 		end, { desc = "Neotest: Toggle summary" })
 
-		vim.keymap.set("n", "<A-7>", function()
-			nt.output_panel.toggle()
+		vim.keymap.set("n", "<leader>tp", function()
+			nt.output.open({ last_run = true, enter = true, short = false })
 		end, { desc = "Neotest: Toggle output panel" })
 
-		vim.keymap.set("n", "<leader>tA", function()
+		vim.keymap.set("n", "<leader>tt", function()
+			nt.run.run()
+		end, { desc = "Test: run nearest" })
+
+		vim.keymap.set("n", "<leader>tf", function()
+			nt.run.run(vim.fn.expand("%"))
+		end, { desc = "Test: run file" })
+
+		vim.keymap.set("n", "<leader>ts", function()
 			nt.run.run({ suite = true })
-		end, { desc = "Neotest: Run ALL tests" })
+		end, { desc = "Test: run suite" })
 
 		vim.keymap.set("n", "<leader>td", function()
 			nt.run.run({ strategy = "dap" })
 		end, { desc = "Neotest: Debug nearest test" })
-
-		end,
+	end,
 }

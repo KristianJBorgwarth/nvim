822768c1a4c72b922041bc43073a783bfb7ceb04
Kristian <10348902@pm.me>
2025-10-12 22:14:46 +0200

    theme: change to dracula

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,32 +1,42 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      flavour = "frappe",
-      color_overrides = {
-        frappe = {
-          base   = "#09090d",
-          mantle = "#0c0c12",
-          crust  = "#111119",
-        },
-      },
-      auto_integrations = true,
-      highlight_overrides = {
-        all = function(C)
-          return require("scripts.neotest-color").get(C)
-        end,
-      },
-    })
+	"Mofiqul/dracula.nvim",
+	name = "dracula",
+	priority = 1000,
+	config = function()
+		require("dracula").setup({
+      colors = {
+        bg = "#09090d",
+        fg = "#c8c3b9",
+        selection = "#44475a",
+        comment = "#6272a4",
+        red = "#ff6ac1",
+        orange = "#ffb86c",
+        yellow = "#f1fa8c",
+        green = "#50fa7b",
+        purple = "#bd93f9",
+        cyan = "#8be9fd",
+        pink = "#ff79c6",
+        bright_red = "#ff6ac1",
+        bright_green = "#69ff94",
+        bright_yellow = "#ffffa5",
+        bright_blue = "#d6acff",
+        bright_magenta = "#ff92df",
+        bright_cyan = "#a4ffff",
+        bright_white = "#ffffff",
+     },
+			highlight_overrides = {
+				all = function(C)
+					return require("scripts.neotest-color").get(C)
+				end,
+			},
+		})
 
-    vim.opt.termguicolors = true
-    vim.cmd.colorscheme("catppuccin")
+		vim.opt.termguicolors = true
+		vim.cmd.colorscheme("dracula")
 
-    vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#1e1e2a" })
-    vim.api.nvim_set_hl(0, "VertSplit",    { fg = "#1e1e2a" })
-    vim.opt.cursorline = true
-    vim.api.nvim_set_hl(0, "CursorLine", { bg = "#111118" })
-  end,
+		vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#1e1e2a" })
+		vim.api.nvim_set_hl(0, "VertSplit", { fg = "#1e1e2a" })
+		vim.opt.cursorline = true
+		vim.api.nvim_set_hl(0, "CursorLine", { bg = "#111118" })
+	end,
 }
-

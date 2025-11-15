57976f379cd91415d4c147b68d8ebbfc3f70d923
Kristian <10348902@pm.me>
2025-11-15 21:58:04 +0100

    formatting

--- lua/plugins/git/neogit.lua
+++ lua/plugins/git/neogit.lua
@@ -24,31 +24,34 @@ return {
 						vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
 					end
 
-			  map("n", "<leader>g", function()
-				  gs.blame_line({ full = true })
-			  end, "Git blame (float)")
+					map("n", "<leader>g", function()
+						gs.blame_line({ full = true })
+					end, "Git blame (float)")
 
-			  map("n", "<leader>gR", gs.reset_buffer, "Git reset buffer")
-		  end,
-	  })
-  end,  },  {
-  "NeogitOrg/neogit",
-  dependencies = {
-	  "nvim-lua/plenary.nvim",
-	  "sindrets/diffview.nvim",
-  },
-  config = function()
-	  local neogit = require("neogit")
+					map("n", "<leader>gR", gs.reset_buffer, "Git reset buffer")
+				end,
+			})
+		end,
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+		},
+		config = function()
+			local neogit = require("neogit")
 
-	  vim.keymap.set(
-		  "n",
-		  "<leader>ga",
-		  "<cmd>!git add .<CR>",
-		  { silent = true, noremap = true, desc = "Git add all" }
-	  )
+			vim.keymap.set(
+				"n",
+				"<leader>ga",
+				"<cmd>!git add .<CR>",
+				{ silent = true, noremap = true, desc = "Git add all" }
+			)
 
-	  vim.keymap.set("n", "<leader>ng", function()
-		  neogit.open()
-	  end, { silent = true, noremap = true, desc = "Open Neogit" })
-  end,  },
+			vim.keymap.set("n", "<leader>ng", function()
+				neogit.open()
+			end, { silent = true, noremap = true, desc = "Open Neogit" })
+		end,
+	},
 }

09596cb6bd63fbbfb95f5338825787a0e6897254
Kristian <10348902@pm.me>
2025-11-15 19:09:56 +0100

    fix clip

--- lua/keybindings/vim-clipboard.lua
+++ lua/keybindings/vim-clipboard.lua
@@ -2,10 +2,10 @@ local map = vim.keymap.set
 vim.opt.clipboard = "unnamedplus"
 
 -- copy to system clipboard
-map({ "v" }, "<leader>y", '"*y', { desc = "Copy to linux system clipboard", silent = true })
+map({ "v" }, "<leader>y", '"+y', { desc = "Copy to linux system clipboard", silent = true })
 
 -- paste from linux system clipboard
-map({ "n", "v" }, "<leader>p", '"*p', { desc = "Paste from linux system clipboard", silent = true })
+map({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from linux system clipboard", silent = true })
 
 -- copy line to system clipboard
 map({ "n" }, "<leader>yy", '"+yy', { desc = "Copy line → clipboard", silent = true })

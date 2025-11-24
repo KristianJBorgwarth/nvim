e1b34297d5f0ebd26a8482a138b1aa55eb113163
Kristian <10348902@pm.me>
2025-11-24 21:17:58 +0100

    improve theme

--- lua/plugins/ui/theme.lua
+++ lua/plugins/ui/theme.lua
@@ -6,24 +6,23 @@ return {
 		require("dracula").setup({
 			colors = {
 				bg = "#09090d",
-				fg = "#c8c3b9",
-				selection = "#44475a",
-				comment = "#6272a4",
-				red = "#ff6ac1",
-				orange = "#ffb86c",
-				yellow = "#f1fa8c",
-				green = "#50fa7b",
-				purple = "#bd93f9",
-				cyan = "#8be9fd",
-				pink = "#ff79c6",
-				bright_red = "#ff6ac1",
-				bright_green = "#69ff94",
-				bright_yellow = "#ffffa5",
-				bright_blue = "#d6acff",
-				bright_magenta = "#ff92df",
-				bright_cyan = "#a4ffff",
-				bright_white = "#ffffff",
-        border = "#1e1e2a",
+				fg = "#d2cdc3",
+				selection = "#3a3d4f",
+				comment = "#707aa8",
+				purple = "#c3a0ff",
+				cyan = "#9adcf7",
+				pink = "#ff86cf",
+				green = "#6ee38f",
+				red = "#ff86cf",
+				orange = "#c3a0ff",
+				bright_red = "#ff9bdc",
+				bright_green = "#86f0a6",
+				bright_yellow = "#cdb8ff",
+				bright_blue = "#cdb8ff",
+				bright_magenta = "#ff9bdc",
+				bright_cyan = "#b6f0ff",
+				bright_white = "#f4f4f4",
+				border = "#1e1e2a",
 			},
 			highlight_overrides = {
 				all = function(C)
@@ -35,16 +34,16 @@ return {
 		vim.opt.termguicolors = true
 		vim.cmd.colorscheme("dracula")
 
+    -- Custom Highlights
 		vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#1e1e2a" })
 		vim.api.nvim_set_hl(0, "VertSplit", { fg = "#1e1e2a" })
 		vim.opt.cursorline = true
 		vim.api.nvim_set_hl(0, "CursorLine", { bg = "#111118" })
 
-
+    -- NeoTree
 		vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "#09090d", fg = "#c8c3b9" })
 		vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "#09090d", fg = "#c8c3b9" })
 		vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { fg = "#1e1e2a" })
 		vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", { fg = "#bd93f9" })
-
 	end,
 }

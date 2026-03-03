5d31ba15bf350ffa19e6cac137f6b64be8df8004
Kristian Borgwarth <10348902@pm.me>
2026-03-03 21:59:05 +0100

    disbale copilot in atlas

--- lua/plugins/ai/copilot.lua
+++ lua/plugins/ai/copilot.lua
@@ -1,16 +1,26 @@
 return {
-	{
+  {
     "github/copilot.vim",
-	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local chat = require("CopilotChat")
-			chat.setup({})
-			vim.keymap.set("n", "<leader>ai", function()
-				chat.toggle()
-			end, { desc = "Copilot Chat" })
-		end,
-	},
+    config = function()
+      local atlas = vim.fn.expand("~/atlas")
+
+      vim.api.nvim_create_autocmd("BufEnter", {
+        callback = function(args)
+          local file = vim.api.nvim_buf_get_name(args.buf)
+          if file:find(atlas, 1, true) == 1 then
+            vim.b.copilot_enabled = false
+          end
+        end,
+      })
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local chat = require("CopilotChat")
+      chat.setup({})
+      vim.keymap.set("n", "<leader>ai", chat.toggle, { desc = "Copilot Chat" })
+    end,
+  },
 }

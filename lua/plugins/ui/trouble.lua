aafa1893201ed337892ca1ba6862447fd61d5972
Kristian Borgwarth <10348902@pm.me>
2026-03-30 00:01:08 +0200

    feat(trouble): removed shit cmds and improved win pos

--- lua/plugins/ui/trouble.lua
+++ lua/plugins/ui/trouble.lua
@@ -1,51 +1,55 @@
 return {
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  cmd = "Trouble",
-  keys = {
-    {
-      "<leader>xx",
-      "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "All diagnostics (workspace)",
-    },
-    {
-      "<leader>xe",
-      "<cmd>Trouble workspace_errors toggle<cr>",
-      desc = "Errors only (workspace)",
-    },
-    {
-      "<leader>xd",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-      desc = "Diagnostics (buffer)",
-    },
-    {
-      "<leader>xq",
-      "<cmd>Trouble quickfix toggle<cr>",
-      desc = "Quickfix",
-    },
-    {
-      "<leader>xl",
-      "<cmd>Trouble loclist toggle<cr>",
-      desc = "Loclist",
-    },
-    {
-      "<leader>xs",
-      "<cmd>Trouble symbols toggle focus=false<cr>",
-      desc = "Document symbols",
-    },
-    {
-      "<leader>xr",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "LSP references/defs/etc",
-    },
-  },
-  opts = {
-    modes = {
-      workspace_errors = {
-        mode = "diagnostics",
-        filter = { severity = vim.diagnostic.severity.ERROR },
-        title = "Workspace Errors",
-      },
-    },
-  },
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "All diagnostics (workspace)",
+		},
+		{
+			"<leader>xe",
+			"<cmd>Trouble workspace_errors toggle<cr>",
+			desc = "Errors only (workspace)",
+		},
+		{
+			"<leader>xd",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Diagnostics (buffer)",
+		},
+		{
+			"<leader>xs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Document symbols",
+		},
+		{
+			"<leader>xr",
+			"<cmd>Trouble lsp_references toggle focus=false<cr>",
+			desc = "LSP references/defs/etc",
+		},
+	},
+	opts = {
+		win = {
+			position = "bottom",
+			height = 10,
+		},
+		modes = {
+			workspace_errors = {
+				mode = "diagnostics",
+				filter = { severity = vim.diagnostic.severity.ERROR },
+				title = "Workspace Errors",
+			},
+			symbols = {
+				win = {
+					position = "bottom",
+				},
+			},
+			lsp = {
+				win = {
+					position = "bottom",
+				},
+			},
+		},
+	},
 }
